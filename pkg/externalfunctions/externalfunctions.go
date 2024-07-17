1a9f6259f15d0057018231d5ba486c832bb7ce0d
Felix Kuhn <120018181+FelixKuhnAnsys@users.noreply.github.com>
2024-07-17 17:16:14 +0200

    add send rest api call to externalfunctions (#39)

--- pkg/externalfunctions/externalfunctions.go
+++ pkg/externalfunctions/externalfunctions.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"regexp"
 	"sort"
 	"strings"
@@ -1518,4 +1519,70 @@ func AnsysGPTGetSystemPrompt(rephrasedQuery string) string {
                 References:
                 [1] Title: "ANSYS HFSS: Antenna Synthesis from HFSS Antenna Toolkit - Part 2", URL: https://ansyskm.ansys.com/forums/topic/ansys-hfss-antenna-synthesis-from-hfss-antenna-toolkit-part-2/, Relevance: 3.53/4.0
                 [2] Title: "Cosimulation Using Ansys HFSS and Circuit - Lesson 2 - ANSYS Innovation Courses", URL: https://courses.ansys.com/index.php/courses/cosimulation-using-ansys-hfss/lessons/cosimulation-using-ansys-hfss-and-circuit-lesson-2/, Relevance: 2.54/4.0`
+}
+
+// SendAPICall sends an API call to the specified URL with the specified headers and query parameters.
+//
+// Parameters:
+//   - requestType: the type of the request (GET, POST, PUT, PATCH, DELETE)
+//   - urlString: the URL to send the request to
+//   - headers: the headers to include in the request
+//   - query: the query parameters to include in the request
+//   - jsonBody: the body of the request as a JSON string
+//
+// Returns:
+//   - success: a boolean indicating whether the request was successful
+//   - returnJsonBody: the JSON body of the response as a string
+func SendRestAPICall(requestType string, endpoint string, header map[string]string, query map[string]string, jsonBody string) (success bool, returnJsonBody string) {
+	// verify correct request type
+	if requestType != "GET" && requestType != "POST" && requestType != "PUT" && requestType != "PATCH" && requestType != "DELETE" {
+		panic(fmt.Sprintf("Invalid request type: %v", requestType))
+	}
+
+	// Parse the URL and add query parameters
+	parsedURL, err := url.Parse(endpoint)
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing URL: %v", err))
+	}
+
+	q := parsedURL.Query()
+	for key, value := range query {
+		q.Add(key, value)
+	}
+	parsedURL.RawQuery = q.Encode()
+
+	// Create the HTTP request
+	var req *http.Request
+	if jsonBody != "" {
+		req, err = http.NewRequest(requestType, parsedURL.String(), bytes.NewBuffer([]byte(jsonBody)))
+	} else {
+		req, err = http.NewRequest(requestType, parsedURL.String(), nil)
+	}
+	if err != nil {
+		panic(fmt.Sprintf("Error creating request: %v", err))
+	}
+
+	// Add headers
+	for key, value := range header {
+		req.Header.Add(key, value)
+	}
+
+	// Execute the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(fmt.Sprintf("Error executing request: %v", err))
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading response body: %v", err))
+	}
+
+	// Check if the response code is successful (2xx)
+	success = resp.StatusCode >= 200 && resp.StatusCode < 300
+
+	return success, string(body)
 }
