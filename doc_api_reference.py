4b189a69c5a92acde418b4c414fbeceac4b75484
laurasgkadri98 <126669056+laurasgkadri98@users.noreply.github.com>
2024-08-09 11:52:05 +0200

    list go pages

--- doc_api_reference.py
+++ doc_api_reference.py
@@ -40,11 +40,11 @@ for root, dirs, files in os.walk(SOURCE_DIRECTORY):
         file_path = os.path.join(root, file)
         try:
             if os.path.isfile(file_path):
-                os.unlink(file_path)
+                print(f'File: {file_path}')
             elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
+                print(f'Directory: {file_path}')
         except Exception as e:
-            print(f'Failed to delete {file_path}. Reason: {e}')
+            print(f'Failed to process {file_path}. Reason: {e}')
 
 # Move the source_directory content to the replacement_directory
 for filename in os.listdir(SOURCE_DIRECTORY):
