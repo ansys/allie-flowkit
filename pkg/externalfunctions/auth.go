3e0e13c6795084b1ea83321f1f0b55bef30139a8
Felix Kuhn <120018181+FelixKuhnAnsys@users.noreply.github.com>
2025-04-16 16:41:18 +0200

    discovery token tracking (#147)

--- pkg/externalfunctions/auth.go
+++ pkg/externalfunctions/auth.go
@@ -55,6 +55,40 @@ func CheckApiKeyAuthMongoDb(apiKey string, mongoDbUrl string, mongoDatabaseName 
 	return true
 }
 
+// CheckCreateUserIdMongoDb checks if a user ID exists in the MongoDB database and creates it if it doesn't.
+//
+// Tags:
+//   - @displayName: Check and Create User ID
+//
+// Parameters:
+//   - userId: The user ID to check.
+//   - tokenLimitForNewUsers: The token limit for new users.
+//   - mongoDbUrl: The URL of the MongoDB database.
+//   - mongoDatabaseName: The name of the MongoDB database.
+//   - mongoDbCollectionName: The name of the MongoDB collection.
+//
+// Returns:
+//   - existingUser: A boolean indicating whether the user ID already exists.
+func CheckCreateUserIdMongoDb(userId string, tokenLimitForNewUsers int, mongoDbUrl string, mongoDatabaseName string, mongoDbCollectionName string) (existingUser bool) {
+
+	// create mongoDb context
+	mongoDbContext, err := mongoDbInitializeClient(mongoDbUrl, mongoDatabaseName, mongoDbCollectionName)
+	if err != nil {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error initializing mongoDb client: %v", err)
+		panic(err)
+	}
+	defer mongoDbContext.Client.Disconnect(context.Background())
+
+	// check if customer for userid exists if not, create it
+	existingUser, _, err = mongoDbGetCreateCustomerByUserId(mongoDbContext, userId, tokenLimitForNewUsers)
+	if err != nil {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error getting or creating customer by userId: %v", err)
+		panic(err)
+	}
+
+	return existingUser
+}
+
 // UpdateTotalTokenCountForCustomerMongoDb updates the total token count for the given customer in the MongoDB database.
 //
 // Tags:
@@ -80,7 +114,7 @@ func UpdateTotalTokenCountForCustomerMongoDb(apiKey string, mongoDbUrl string, m
 	defer mongoDbContext.Client.Disconnect(context.Background())
 
 	// update token count
-	err = mongoDbAddToTotalTokenCount(mongoDbContext, apiKey, additionalTokenCount)
+	err = mongoDbAddToTotalTokenCount(mongoDbContext, "api_key", apiKey, additionalTokenCount)
 	if err != nil {
 		logging.Log.Errorf(&logging.ContextMap{}, "Error updating total token count for customer: %v", err)
 		panic(err)
@@ -88,6 +122,50 @@ func UpdateTotalTokenCountForCustomerMongoDb(apiKey string, mongoDbUrl string, m
 
 	// check if customer is over the limit
 	exists, customer, err := mongoDbGetCustomerByApiKey(mongoDbContext, apiKey)
+	if err != nil || !exists {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error getting customer by API key: %v", err)
+		panic(err)
+	}
+	if customer.TotalTokenCount >= customer.TokenLimit {
+		return true
+	}
+
+	return false
+}
+
+// UpdateTotalTokenCountForUserIdMongoDb updates the total token count for the given user ID in the MongoDB database.
+//
+// Tags:
+//   - @displayName: Update Total Token Count by User ID
+//
+// Parameters:
+//   - userId: The user ID of the customer.
+//   - mongoDbUrl: The URL of the MongoDB database.
+//   - mongoDatabaseName: The name of the MongoDB database.
+//   - mongoDbCollectionName: The name of the MongoDB collection.
+//   - additionalTokenCount: The number of additional tokens to add to the total token count.
+//
+// Returns:
+//   - tokenLimitReached: A boolean indicating whether the customer has reached the token limit.
+func UpdateTotalTokenCountForUserIdMongoDb(userId string, mongoDbUrl string, mongoDatabaseName string, mongoDbCollectionName string, additionalTokenCount int) (tokenLimitReached bool) {
+
+	// create mongoDb context
+	mongoDbContext, err := mongoDbInitializeClient(mongoDbUrl, mongoDatabaseName, mongoDbCollectionName)
+	if err != nil {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error initializing mongoDb client: %v", err)
+		panic(err)
+	}
+	defer mongoDbContext.Client.Disconnect(context.Background())
+
+	// update token count
+	err = mongoDbAddToTotalTokenCount(mongoDbContext, "user_id", userId, additionalTokenCount)
+	if err != nil {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error updating total token count for customer: %v", err)
+		panic(err)
+	}
+
+	// check if customer is over the limit
+	exists, customer, err := mongoDbGetCreateCustomerByUserId(mongoDbContext, userId, 0)
 	if err != nil || !exists {
 		logging.Log.Errorf(&logging.ContextMap{}, "Error getting customer by API key: %v", err)
 		panic(err)
@@ -133,13 +211,55 @@ func DenyCustomerAccessAndSendWarningMongoDb(apiKey string, mongoDbUrl string, m
 	}
 
 	// deny customer access and set warning sent
-	err = mongoDbUpdateAccessAndWarning(mongoDbContext, apiKey)
+	err = mongoDbUpdateAccessAndWarning(mongoDbContext, "api_key", apiKey)
 	if err != nil {
 		logging.Log.Errorf(&logging.ContextMap{}, "Error updating access and warning for customer: %v", err)
 		panic(err)
 	}
 
 	return customer.CustomerName, sendWarning
+}
+
+// DenyCustomerAccessAndSendWarningMongoDbUserId denies access to the customer by user ID and sends a warning if necessary.
+//
+// Tags:
+//   - @displayName: Deny Customer Access by User ID
+//
+// Parameters:
+//   - userId: The user ID of the customer.
+//   - mongoDbUrl: The URL of the MongoDB database.
+//   - mongoDatabaseName: The name of the MongoDB database.
+//   - mongoDbCollectionName: The name of the MongoDB collection.
+//
+// Returns:
+//   - sendWarning: A boolean indicating whether a warning should be sent to the customer.
+func DenyCustomerAccessAndSendWarningMongoDbUserId(userId string, mongoDbUrl string, mongoDatabaseName string, mongoDbCollectionName string) (sendWarning bool) {
+	// create mongoDb context
+	mongoDbContext, err := mongoDbInitializeClient(mongoDbUrl, mongoDatabaseName, mongoDbCollectionName)
+	if err != nil {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error initializing mongoDb client: %v", err)
+		panic(err)
+	}
+	defer mongoDbContext.Client.Disconnect(context.Background())
+
+	// check if warning for customer needs to be sent
+	exists, customer, err := mongoDbGetCreateCustomerByUserId(mongoDbContext, userId, 0)
+	if err != nil || !exists {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error getting customer by API key: %v", err)
+		panic(err)
+	}
+	if !customer.WarningSent {
+		sendWarning = true
+	}
+
+	// deny customer access and set warning sent
+	err = mongoDbUpdateAccessAndWarning(mongoDbContext, "user_id", userId)
+	if err != nil {
+		logging.Log.Errorf(&logging.ContextMap{}, "Error updating access and warning for customer: %v", err)
+		panic(err)
+	}
+
+	return sendWarning
 }
 
 // SendLogicAppNotificationEmail sends a POST request to the email service.
