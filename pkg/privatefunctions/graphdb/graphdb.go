3862a1931779f19c589d8336597f6a97c4d2beea
FelixKuhnAnsys <felix.kuhn@ansys.com>
2025-05-13 17:32:52 +0200

    Update graphdb.go

--- pkg/privatefunctions/graphdb/graphdb.go
+++ pkg/privatefunctions/graphdb/graphdb.go
@@ -31,9 +31,9 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/ansys/aali-flowkit/pkg/privatefunctions/codegeneration"
 	"github.com/ansys/aali-graphdb-goclient/aali_graphdb"
 
+	"github.com/ansys/aali-flowkit/pkg/privatefunctions/codegeneration"
 	"github.com/ansys/aali-sharedtypes/pkg/logging"
 	"github.com/ansys/aali-sharedtypes/pkg/sharedtypes"
 )
