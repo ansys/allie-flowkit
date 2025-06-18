d482d173e326a5021319131fc13200fded85a2ca
FelixKuhnAnsys <felix.kuhn@ansys.com>
2025-06-18 08:52:52 +0200

    unit tests

--- pkg/externalfunctions/privatefunctions.go
+++ pkg/externalfunctions/privatefunctions.go
@@ -1941,62 +1941,6 @@ func convertToFloat32Slice(interfaceSlice []interface{}) ([]float32, error) {
 	return float32Slice, nil
 }
 
-// createPayloadAndSendHttpRequest creates a JSON payload and sends an HTTP POST request.
-//
-// Parameters:
-//   - url: the URL to send the request to.
-//   - requestObject: the object to send in the request body.
-//   - responsePtr: a pointer to the object to store the response in.
-//
-// Returns:
-//   - error: the error returned by the function.
-func createPayloadAndSendHttpRequest(url string, requestObject interface{}, responsePtr interface{}) (funcError error, statusCode int) {
-	defer func() {
-		r := recover()
-		if r != nil {
-			logging.Log.Errorf(&logging.ContextMap{}, "Panic in CreatePayloadAndSendHttpRequest: %v", r)
-			funcError = r.(error)
-			return
-		}
-	}()
-
-	// Define the JSON payload.
-	jsonPayload, err := json.Marshal(requestObject)
-	if err != nil {
-		return fmt.Errorf("error marshalling JSON payload: %v", err), 0
-	}
-
-	// Create a new HTTP POST request.
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		return err, 0
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-
-	// Send the HTTP POST request.
-	resp, err := client.Do(req)
-	if err != nil {
-		return err, resp.StatusCode
-	}
-	defer resp.Body.Close()
-
-	// Decode the JSON response body into the 'data' struct.
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(responsePtr); err != nil {
-		return err, 0
-	}
-
-	// Check the response status code.
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf(resp.Status), resp.StatusCode
-	}
-
-	return nil, 0
-}
-
 // openAiTokenCount returns the number of tokens in a message for a given model.
 //
 // Parameters:
