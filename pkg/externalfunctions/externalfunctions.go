f788caad907bc0cf872ee611f7fb76a6dda2afbf
David Cha <david.cha@ansys.com>
2025-06-20 11:50:20 +0100

    Fix code style;

--- pkg/externalfunctions/externalfunctions.go
+++ pkg/externalfunctions/externalfunctions.go
@@ -154,13 +154,13 @@ var ExternalFunctionsMap = map[string]interface{}{
 	"GetSystemPrompt": GetSystemPrompt,
 
 	// materials
-	"SerializeResponse":              	SerializeResponse,
-	"AddGuidsToAttributes":           	AddGuidsToAttributes,
-	"FilterOutNonExistingAttributes": 	FilterOutNonExistingAttributes,
-	"FilterOutDuplicateAttributes":   	FilterOutDuplicateAttributes,
-	"ExtractCriteriaSuggestions":     	ExtractCriteriaSuggestions,
-	"LogRequestSuccess": 			  	LogRequestSuccess,
-	"LogRequestFailed": 			  	LogRequestFailed,
+	"SerializeResponse":                SerializeResponse,
+	"AddGuidsToAttributes":             AddGuidsToAttributes,
+	"FilterOutNonExistingAttributes":   FilterOutNonExistingAttributes,
+	"FilterOutDuplicateAttributes":     FilterOutDuplicateAttributes,
+	"ExtractCriteriaSuggestions":       ExtractCriteriaSuggestions,
+	"LogRequestSuccess":                LogRequestSuccess,
+	"LogRequestFailed":                 LogRequestFailed,
 	"LogRequestFailedDebugWithMessage": LogRequestFailedDebugWithMessage,
 	"PerformMultipleGeneralRequestsAndExtractAttributesWithOpenAiTokenOutput": PerformMultipleGeneralRequestsAndExtractAttributesWithOpenAiTokenOutput,
 
